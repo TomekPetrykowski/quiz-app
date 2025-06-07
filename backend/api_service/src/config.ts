ee13e15cf3f243ad65db4a655e7fc20262ff87ce
Tomek Petrykowski <tomasz.petrykowski.20@gmail.com>
2025-06-07 23:48:11 +0200

    feat: Add custom error handling middleware with custom errors

--- backend/api_service/src/config.ts
+++ backend/api_service/src/config.ts
@@ -1,6 +1,7 @@
 const config = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
+  debug: process.env.APP_DEBUG === "true",
 };
 
 export default config;
--- backend/api_service/src/routes/v1/placeholder/controller.ts
+++ backend/api_service/src/routes/v1/placeholder/controller.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from "express";
+import EntityNotFoundError from "../../../errors/EntityNotFoundError";
 
 export const someFunc = (req: Request, res: Response): void => {
+  // throw new Error("This is an error");
+  throw new EntityNotFoundError({
+    message: "Aqq",
+    code: "ERR_NF",
+    statusCode: 404,
+  });
   res.status(200).json({
     message: "Hello, world!",
   });
